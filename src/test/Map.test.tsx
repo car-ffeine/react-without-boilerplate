03997ce422b60f340d1fb759764067cafa778bf2
kyw0716 <kyw0716@naver.com>
2023-07-04 22:53:54 +0900

    test: Jest 예시 코드 추가

---
+++ src/test/Map.test.tsx
@@ -0,0 +1,14 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import { expect } from '@jest/globals';
+
+import ComponentForTest from '../components/ComponentForTest';
+
+it('테스트 문구', async () => {
+  const { container } = render(<ComponentForTest />);
+
+  fireEvent.click(screen.getByText('ㅎㅇㅎㅇ'));
+
+  const button = container.getElementsByTagName('button')[0];
+
+  expect(button.textContent).toBe('하이하이');
+});
