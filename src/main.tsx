474516c4b2cecde94b0e2a17e4dfc53e01c7aa7e
kyw0716 <kyw0716@naver.com>
2023-07-12 12:27:05 +0900

    chore: react-query, react-query-devtools 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import App from './app';
 import { createRoot } from 'react-dom/client';
 import { worker } from './mocks/browser';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
+
+const queryClient = new QueryClient();
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
 
-root.render(<App />);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+);
